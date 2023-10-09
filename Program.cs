50a463aa80fc92f0bd41f30cd52f3845ef6263e4
Lenovo-User <Lenovo-User@DESKTOP-Q92463H>
2023-10-09 12:00:38 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Working_with_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5);
+            Console.WriteLine(6+5);
+            Console.WriteLine(5 & 2);
+            Console.WriteLine(4 + 1 * 2);
+            Console.WriteLine(5 / 2);
+            Console.WriteLine(5/2.0);
+            int num = 6;
+            num++;
+            Console.WriteLine(num);
+            Console.WriteLine(Math.Abs(-40));
+            Console.WriteLine(Math.Pow(2,2));
+        }
+    }
+}
